d42fdedb3d570e8d719f43e0a77fbcf3680fd702
Nathan Krasney <natankrasney@gmail.com>
2022-12-28 09:31:38 +0200

    add .gitignore

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const utils_1 = require("./utils");
-console.log(utils_1.factorial(3));
-//# sourceMappingURL=index.js.map
